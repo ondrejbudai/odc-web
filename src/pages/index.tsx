456189497531d80dc23634f680cdb0905c98b20d
Ondřej Budai <ondrej@budai.cz>
2023-11-25 15:04:52 +0100

    index: bring back the link to the tutorial in the header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,14 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Build your first bootable image! 🚀
+          </Link>
+        </div>
+
       </div>
     </header>
   );
